ec6a0784a9a2c4b7ce442625d75d3288efe4d819
raaghulr <raaghulrkgisl@gmail.com>
2015-06-23 13:21:43 +0530

    First Upload
    First Upload of Project

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+	public Transform[] patrolpoints;
+	public float movespeed;
+	public int currentpoint;
+
+
+	void Start()
+	{
+		transform.position = patrolpoints [0].position;
+		currentpoint = 0;
+	}
+
+	void Update()
+	{
+		if (transform.position == patrolpoints [currentpoint].position) {
+			currentpoint++;
+		}
+		if (currentpoint >= patrolpoints.Length)
+			currentpoint = 0;
+		transform.position = Vector3.MoveTowards (transform.position, patrolpoints [currentpoint].position, movespeed * Time.deltaTime);
+	}
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public Rigidbody player;
+	Vector3  input ;
+	public float movespeed=10;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		input = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+		if (player.velocity.magnitude < 10) {
+			player.AddForce (input * movespeed);
+		}
+	}
+}
